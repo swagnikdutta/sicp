ea847dea4c47c7204f4a51c3f457bc3ca34f4c20
Swagnik Dutta <swagnikd@gmail.com>
2023-09-12 00:16:58 +0530

    solved exercise 1.3

--- chapter 1/exercise-1.3.rkt
+++ chapter 1/exercise-1.3.rkt
@@ -7,7 +7,30 @@
 (define (square x)
   (* x x))
 
-(square 4)
+(define (find-smallest x y z)
+  (cond ((and (<= x y) (<= x z)) x)
+        ((and (<= y x) (<= y z)) y)
+        (else z)))
 
-;(define (sum-of-squares x y z)
-  
+(define (sum-of-two-largest-squares x y z)
+  (define smallest (find-smallest x y z))
+  (define sum-of-three-squares (+ (square x) (square y) (square z)))
+  (- sum-of-three-squares (square smallest)))
+
+(define (sum-of-two-larger-squares-v2 x y z)
+  (define largest (max x y z))
+  (define second-largest (cond ((or
+                                 (and (<= x y) (<= y z))
+                                 (and (<= z y) (<= y x))) y)
+                               ((or
+                                 (and (<= y x) (<= x z))
+                                 (and (<= z x) (<= x y))) x)
+                               (else z)))
+  (+ (square largest) (square second-largest)))
+
+(define x 5)
+(define y 7)
+(define z 6)
+
+(sum-of-two-largest-squares x y z)
+(sum-of-two-larger-squares-v2 x y z)
